d77408ec0f5360342fc4abb969d4fd832332ab63
Ahmed.net <Ahmed.net@gmail.com>
2023-03-27 04:06:56 +0200

    Adding project

---
+++ Moving slider/app.js
@@ -0,0 +1,42 @@
+let img1=document.getElementById('press1');
+let img2=document.getElementById('press2');
+let img3=document.getElementById('press3');
+let img4=document.getElementById('press4');
+
+let img11=document.getElementById('press11');
+let img22=document.getElementById('press22');
+let img33=document.getElementById('press33');
+let img44=document.getElementById('press44');
+
+img1.onclick= function(){
+    main.src= this.src;
+}
+
+img2.onclick= function(){
+    main.src= this.src;
+}
+
+img3.onclick= function(){
+    main.src= this.src;
+}
+
+img4.onclick= function(){
+    main.src= this.src;
+}
+
+img11.onmouseover= function(){
+    main.src= this.src;
+}
+
+img22.onmouseover= function(){
+    main.src= this.src;
+}
+
+img33.onmouseover= function(){
+    main.src= this.src;
+}
+
+img44.onmouseover= function(){
+    main.src= this.src;
+}
+
